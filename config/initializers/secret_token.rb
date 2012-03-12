152aef9a0185aabccd64e770cf7d834373e66565
Cyrille Labesse <cyrille.labesse@gmail.com>
2012-03-12 01:35:27 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+California::Application.config.secret_token = '21b818b86be5bad6697242cc24561904df5e505b03befee0320ca4c663ebe37353ea6be7ef617fdb804e141f2990e5546eff13e3326e462cff1f07b4e7cc35a1'
